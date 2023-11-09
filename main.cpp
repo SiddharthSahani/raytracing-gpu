e411fe0bca8b2cc9d71c6085cd3e7c73f954c5da
Siddharth Sahani <sidhucroedde@gmail.com>
2023-11-09 08:41:30 +0530

    Moved cl stuff in main.cpp to a new file

--- main.cpp
+++ main.cpp
@@ -1,96 +1,13 @@
 
 #include "src/camera.h"
-#include <CL/opencl.hpp>
+#include "src/cl_utils.h"
 #include <raylib/raylib.h>
-#include <vector>
-#include <fstream>
 
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 #define IMAGE_WIDTH (1280/2)
 #define IMAGE_HEIGHT (720/2)
-
-
-bool get_device(cl::Device& selected_device) {
-    std::vector<cl::Platform> all_platforms;
-    cl::Platform::get(&all_platforms);
-
-    std::vector<cl::Device> all_devices;
-    for (const auto& plat : all_platforms) {
-        std::vector<cl::Device> temp;
-        plat.getDevices(CL_DEVICE_TYPE_ALL, &temp);
-        all_devices.insert(all_devices.end(), temp.begin(), temp.end());
-    }
-
-    if (all_devices.size() == 0) {
-        printf("No OpenCl compactible device found\n");
-        return false;
-    }
-    if (all_devices.size() == 1) {
-        selected_device = all_devices[0];
-        return true;
-    }
-
-    printf("Multiple devices found, choose one:\n");
-    for (int i = 0; i < all_devices.size(); i++) {
-        const auto& device = all_devices[i];
-        printf("%d) %s [%d CU]\n", i+1, device.getInfo<CL_DEVICE_NAME>().c_str(), device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>());
-    }
-
-    int selected_device_idx;
-    printf("Enter selection: ");
-    scanf("%d", &selected_device_idx);
-
-    selected_device = all_devices[selected_device_idx-1];
-    return true;
-}
-
-
-void print_device_info(cl::Device& device) {
-    auto cu_count = device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>();
-    auto clock_freq = device.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>();
-    auto dev_name = device.getInfo<CL_DEVICE_NAME>();
-    auto version = device.getInfo<CL_DEVICE_VERSION>();
-    auto cl_version = device.getInfo<CL_DEVICE_OPENCL_C_VERSION>();
-
-    printf("----- Selected Device Info -----\n");
-    printf("  Name: %s\n", dev_name.c_str());
-    printf("  Version: %s\n", version.c_str());
-    printf("  OpenCl C Version: %s\n", cl_version.c_str());
-    printf("  Compute Units: %d\n", cu_count);
-    printf("  Clock Frequency: %d\n", clock_freq);
-}
-
-
-std::string read_file(const char* filepath) {
-    std::ifstream file(filepath, std::ios::in | std::ios::ate);
-
-    int file_size = file.tellg();
-    std::string file_content(file_size, '\0');
-
-    file.seekg(0);
-    file.read(&file_content[0], file_size);
-    return file_content;
-}
-
-
-bool create_opencl_objects(const cl::Device& device, cl::Context& context, cl::CommandQueue& queue, cl::Program& program) {
-    context = cl::Context(device);
-    queue = cl::CommandQueue(context, device);
-
-    std::string program_string = read_file("kernels/renderer.cl");
-    program = cl::Program(program_string);
-
-    if (program.build()) {
-        printf("Unable to build OpenCl program\n");
-        std::string build_log = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device);
-        printf("Build Log:\n%s\n", build_log.c_str());
-        return false;
-    }
-
-    return true;
-}
 
 
 void render_test_scene(const cl::Context& context, const cl::CommandQueue& queue, cl::Program& program, std::vector<glm::vec4>& pixels_h) {
---
+++ src/cl_utils.h
@@ -0,0 +1,87 @@
+
+#pragma once
+
+#include <CL/opencl.hpp>
+#include <vector>
+#include <fstream>
+
+
+bool get_device(cl::Device& selected_device) {
+    std::vector<cl::Platform> all_platforms;
+    cl::Platform::get(&all_platforms);
+
+    std::vector<cl::Device> all_devices;
+    for (const auto& plat : all_platforms) {
+        std::vector<cl::Device> temp;
+        plat.getDevices(CL_DEVICE_TYPE_ALL, &temp);
+        all_devices.insert(all_devices.end(), temp.begin(), temp.end());
+    }
+
+    if (all_devices.size() == 0) {
+        printf("No OpenCl compactible device found\n");
+        return false;
+    }
+    if (all_devices.size() == 1) {
+        selected_device = all_devices[0];
+        return true;
+    }
+
+    printf("Multiple devices found, choose one:\n");
+    for (int i = 0; i < all_devices.size(); i++) {
+        const auto& device = all_devices[i];
+        printf("%d) %s [%d CU]\n", i+1, device.getInfo<CL_DEVICE_NAME>().c_str(), device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>());
+    }
+
+    int selected_device_idx;
+    printf("Enter selection: ");
+    scanf("%d", &selected_device_idx);
+
+    selected_device = all_devices[selected_device_idx-1];
+    return true;
+}
+
+
+void print_device_info(cl::Device& device) {
+    auto cu_count = device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>();
+    auto clock_freq = device.getInfo<CL_DEVICE_MAX_CLOCK_FREQUENCY>();
+    auto dev_name = device.getInfo<CL_DEVICE_NAME>();
+    auto version = device.getInfo<CL_DEVICE_VERSION>();
+    auto cl_version = device.getInfo<CL_DEVICE_OPENCL_C_VERSION>();
+
+    printf("----- Selected Device Info -----\n");
+    printf("  Name: %s\n", dev_name.c_str());
+    printf("  Version: %s\n", version.c_str());
+    printf("  OpenCl C Version: %s\n", cl_version.c_str());
+    printf("  Compute Units: %d\n", cu_count);
+    printf("  Clock Frequency: %d\n", clock_freq);
+}
+
+
+std::string read_file(const char* filepath) {
+    std::ifstream file(filepath, std::ios::in | std::ios::ate);
+
+    int file_size = file.tellg();
+    std::string file_content(file_size, '\0');
+
+    file.seekg(0);
+    file.read(&file_content[0], file_size);
+    return file_content;
+}
+
+
+bool create_opencl_objects(const cl::Device& device, cl::Context& context, cl::CommandQueue& queue, cl::Program& program) {
+    context = cl::Context(device);
+    queue = cl::CommandQueue(context, device);
+
+    std::string program_string = read_file("kernels/renderer.cl");
+    program = cl::Program(program_string);
+
+    if (program.build()) {
+        printf("Unable to build OpenCl program\n");
+        std::string build_log = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device);
+        printf("Build Log:\n%s\n", build_log.c_str());
+        return false;
+    }
+
+    return true;
+}
