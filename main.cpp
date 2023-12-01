bd45f00caeab8fbaae6bd4406d581c280d8cc386
Siddharth Sahani <sidhucroedde@gmail.com>
2023-12-01 22:39:28 +0530

    Added utility to time funcs (benchmarking reasons)

--- main.cpp
+++ main.cpp
@@ -2,12 +2,49 @@
 #define RT_LOG printf
 #include "src/renderer.h"
 #include "src/test_scenes.h"
+#include <chrono>
+
+using namespace std::chrono;
 
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 #define IMAGE_WIDTH (1280/2)
 #define IMAGE_HEIGHT (720/2)
+
+
+#define TIME_FUNCTION(name, func) \
+{\
+    Timer timer(name);\
+    func;\
+}
+
+
+// utility class to time functions
+class Timer {
+
+    public:
+        Timer(const char* timerName) {
+            m_timerName = timerName;
+            m_startTime = high_resolution_clock::now();
+        }
+        ~Timer() {
+            auto stopTime = high_resolution_clock::now();
+            auto timeTaken = stopTime - m_startTime;
+            auto timeTaken_ns = timeTaken.count();
+            printf(
+                "%s took %f secs (%f ms)\n",
+                m_timerName,
+                (float) timeTaken_ns / 1'000'000'000,
+                (float) timeTaken_ns / 1'000'000
+            );
+        }
+
+    private:
+        const char* m_timerName;
+        system_clock::time_point m_startTime;
+
+};
 
 
 int main() {
@@ -25,7 +62,7 @@ int main() {
         ClearBackground(RAYWHITE);
 
         if (IsKeyPressed(KEY_SPACE)) {
-            raytracer.renderScene(scene, camera, rt::DEFAULT_CONFIG);
+            TIME_FUNCTION("renderScene", raytracer.renderScene(scene, camera, rt::DEFAULT_CONFIG));
             renderer.update();
         }
 
