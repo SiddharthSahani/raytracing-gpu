9f5062f6ec7073226d779a1d212058eba4d381f6
Siddharth Sahani <sidhucroedde@gmail.com>
2024-02-01 02:21:43 +0530

    Better raylib renderer class

---
+++ src/renderers/raylibrenderer.cpp
@@ -0,0 +1,61 @@
+
+#include "src/renderers/raylibrenderer.h"
+#include "src/raytracer.h"
+
+
+using namespace rt;
+
+
+static Texture createRaylibTexture(uint32_t width, uint32_t height, Format format) {
+    PixelFormat rlFormat;
+
+    switch (format) {
+        case Format::RGBA8:
+            rlFormat = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
+            break;
+        case Format::RGBA32F:
+            rlFormat = PIXELFORMAT_UNCOMPRESSED_R32G32B32A32;
+            break;
+        default:
+            RT_LOG("No corresponding format available in raylib");
+            break;
+    }
+
+    Image tempImage = GenImageColor(width, height, DARKBLUE);
+    ImageFormat(&tempImage, rlFormat);
+    Texture out = LoadTextureFromImage(tempImage);
+    UnloadImage(tempImage);
+    return out;
+}
+
+
+RaylibRenderer::RaylibRenderer(const Raytracer& raytracer, glm::ivec2 windowSize, int targetFps)
+: m_raytracer(raytracer), m_windowSize(windowSize) {
+    SetTraceLogLevel(LOG_NONE);
+    InitWindow(windowSize.x, windowSize.y, "Raytracing [with Raylib]");
+    SetTargetFPS(targetFps);
+
+    uint32_t bufferSize = m_raytracer.getPixelBufferSize();
+    m_outBuffer = new uint8_t[bufferSize];
+    const glm::ivec2& imgShape = m_raytracer.getImageShape();
+    m_outTexture = createRaylibTexture(imgShape.x, imgShape.y, raytracer.getPixelFormat());
+}
+
+
+RaylibRenderer::~RaylibRenderer() {
+    delete[] m_outBuffer;
+    UnloadTexture(m_outTexture);
+    CloseWindow();
+}
+
+
+void RaylibRenderer::update() {
+    m_raytracer.readPixels(m_outBuffer);
+    UpdateTexture(m_outTexture, m_outBuffer);
+}
+
+
+void RaylibRenderer::draw() {
+    float scale = (float) m_windowSize.x / (float) m_outTexture.width;
+    DrawTextureEx(m_outTexture, {0.0f, 0.0f}, 0.0f, scale, WHITE);
+}
--- src/renderers/raylibrenderer.h
+++ src/renderers/raylibrenderer.h
@@ -1,63 +1,30 @@
 
 #pragma once
 
-#include "src/raytracer.cpp"
 #include <raylib/raylib.h>
+#include <glm/glm.hpp>
 
 
 namespace rt {
 
-static Texture createRaylibTexture(uint32_t width, uint32_t height, Format format) {
-    Image tempImage = GenImageColor(width, height, BLACK);
-    ImageFormat(&tempImage, format == Format::RGBA8 ? PIXELFORMAT_UNCOMPRESSED_R8G8B8A8 : PIXELFORMAT_UNCOMPRESSED_R32G32B32A32);
-    Texture texture = LoadTextureFromImage(tempImage);
-    UnloadImage(tempImage);
-    return texture;
-}
+
+class Raytracer;
 
 
 class RaylibRenderer {
 
     public:
-        RaylibRenderer(const Raytracer& raytracer);
+        RaylibRenderer(const Raytracer& raytracer, glm::ivec2 windowSize, int targetFps);
         ~RaylibRenderer();
-        void draw(float scale) const;
         void update();
+        void draw();
 
     private:
         const Raytracer& m_raytracer;
+        glm::ivec2 m_windowSize;
         uint8_t* m_outBuffer;
         Texture m_outTexture;
 
 };
-
-
-RaylibRenderer::RaylibRenderer(const Raytracer& raytracer)
-: m_raytracer(raytracer) {
-    uint32_t bufferSize = m_raytracer.getPixelBufferSize();
-    m_outBuffer = new uint8_t[bufferSize];
-
-    glm::ivec2 shape = m_raytracer.getImageShape();
-    uint32_t width = shape.x;
-    uint32_t height = shape.y;
-    m_outTexture = createRaylibTexture(width, height, raytracer.getPixelFormat());
-}
-
-
-RaylibRenderer::~RaylibRenderer() {
-    delete[] m_outBuffer;
-    UnloadTexture(m_outTexture);
-}
-
-
-void RaylibRenderer::draw(float scale) const {
-    DrawTextureEx(m_outTexture, {0.0f, 0.0f}, 0.0f, scale, WHITE);
-}
-
-
-void RaylibRenderer::update() {
-    m_raytracer.readPixels(m_outBuffer);
-    UpdateTexture(m_outTexture, m_outBuffer);
-}
 
 }
