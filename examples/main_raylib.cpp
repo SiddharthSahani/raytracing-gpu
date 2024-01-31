3e3b1e4db5e276d92399ff1e5199bccb18b85540
Siddharth Sahani <sidhucroedde@gmail.com>
2024-02-01 02:22:10 +0530

    Demo using the raylib renderer

--- examples/main_raylib.cpp
+++ examples/main_raylib.cpp
@@ -1,36 +1,15 @@
 
 #define RT_PRINT_LOG
 
-#include "src/renderers/raylibrenderer.h"
+#include "src/raytracer.cpp"
+#include "src/renderers/raylibrenderer.cpp"
 #include "src/test_scenes.h"
-
-
-#define WINDOW_WIDTH (1280)
-#define WINDOW_HEIGHT (720)
-#define IMAGE_SCALE (2.0f)
-#define IMAGE_WIDTH (WINDOW_WIDTH / IMAGE_SCALE)
-#define IMAGE_HEIGHT (WINDOW_HEIGHT / IMAGE_SCALE)
-
-
-std::vector<rt::internal::Scene> createAllScenes(cl::Context context, cl::CommandQueue queue) {
-    std::vector<rt::Scene> scenes = {
-        createScene_1(),
-        createScene_2(),
-        createScene_3(),
-        createScene_4(),
-        createScene_5(),
-    };
-    std::vector<rt::internal::Scene> res;
-    for (const auto& scene : scenes) {
-        res.push_back(scene.convert(context, queue));
-    }
-    return res;
-}
 
 
 bool isSceneChanged() {
     return IsKeyDown(KEY_C) && (IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_RIGHT));
 }
+
 
 int getSceneIndex(int sceneCount) {
     static int sceneIndex = 0;
@@ -42,45 +21,65 @@ int getSceneIndex(int sceneCount) {
 }
 
 
-int main() {
-    SetTraceLogLevel(LOG_NONE);
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Raytracing with OpenCl");
-    SetTargetFPS(30);
+int main(int argc, char* argv[]) {
+    // to select preffered gpu
+    const int clPlatformIdx = 0;
+    const int clDeviceIdx = 0;
+    // window and image size
+    const int displayWidth = 1280;
+    const int displayHeight = 720;
+    const float scale = 2.0f;
+    const int imageWidth = displayWidth / scale;
+    const int imageHeight = displayHeight / scale;
+    // kernel and display timers
+    const int displayUpdatesPerSec = 1;
+    const int kernelExecsPerSec = 30;
+    // raytracing config
+    const rt::Config rtConfig = {
+        .sampleCount = 16, .bounceLimit = 5
+    };
 
-    cl::Platform platform = rt::getAllClPlatforms()[0];
-    cl::Device device = rt::getAllClDevices(platform)[0];
-    rt::CL_Objects clObjects = rt::createClObjects(platform, device);
+    cl::Platform platform = rt::getAllClPlatforms()[clPlatformIdx];
+    cl::Device device = rt::getAllClDevices(platform)[clDeviceIdx];
+    rt::CL_Objects clObj = rt::createClObjects(platform, device);
 
-    rt::Raytracer raytracer({IMAGE_WIDTH, IMAGE_HEIGHT}, clObjects, rt::Format::RGBA32F, true);
-    rt::RaylibRenderer renderer(raytracer);
+    rt::Raytracer raytracer({imageWidth, imageHeight}, clObj, rt::Format::RGBA8, true);
+    rt::RaylibRenderer renderer(raytracer, {displayWidth, displayHeight}, kernelExecsPerSec);
 
-    auto camera = rt::createCamera(60.0f, {IMAGE_WIDTH, IMAGE_HEIGHT}, {0, 0, 6}, {0, 0, -1});
-    auto scenes = createAllScenes(clObjects.context, clObjects.queue);
+    auto camera = rt::createCamera(60.0f, {imageWidth, imageHeight}, {0, 0, 6}, {0, 0, -1});
+    auto scenes = createAllScenes(clObj.context, clObj.queue);
     int numScenes = scenes.size();
 
+    int sceneIdx = 0;
+    raytracer.renderScene(scenes[sceneIdx], camera, rtConfig);
+
+    int displayUpdateCount = 0;
+    int kernelExecCount = -1;
+
     while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        if (IsKeyPressed(KEY_S) && IsKeyDown(KEY_LEFT_CONTROL)) {
-            RT_LOG("Image save status: %s", raytracer.saveAsImage("result.png") ? "true" : "false");
-        }
-
         if (isSceneChanged()) {
             raytracer.resetFrameCount();
+            sceneIdx = getSceneIndex(numScenes);
+            displayUpdateCount = 0;
+            kernelExecCount = 0;
         }
 
-        if (IsKeyPressed(KEY_SPACE) || isSceneChanged()) {
-            auto curScene = scenes[getSceneIndex(numScenes)];
-            RT_TIME_STMT("renderScene", raytracer.renderScene(curScene, camera, {.sampleCount = 32, .bounceLimit = 5}));
-            RT_TIME_STMT("accumulatePixels", raytracer.accumulatePixels());
+        kernelExecCount++;
+        raytracer.renderScene(scenes[sceneIdx], camera, rtConfig);
+        raytracer.accumulatePixels();
+
+        if (kernelExecCount % (kernelExecsPerSec / displayUpdatesPerSec) == 0 || isSceneChanged()) {
+            displayUpdateCount++;
             renderer.update();
         }
 
-        renderer.draw(IMAGE_SCALE);
-
-        DrawFPS(10, 10);
-        DrawText(TextFormat("Frame time: %f", GetFrameTime()), 10, 30, 18, BLACK);
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        renderer.draw();
+        DrawText(TextFormat("Kernel Exec Count: %d", kernelExecCount), 10, 10, 18, GREEN);
+        DrawText(TextFormat("Display Update Count: %d", displayUpdateCount), 10, 30, 18, GREEN);
+        DrawText(TextFormat("Samples per pixel: %d", rtConfig.sampleCount * kernelExecCount), 10, 50, 18, GREEN);
+        DrawFPS(10, 70);
         EndDrawing();
     }
 }
