d5e876a83a109f3c47362a56d2c95321b4c3d70d
Siddharth Sahani <sidhucroedde@gmail.com>
2024-02-08 20:14:12 +0530

    Added more test scenes

--- src/test_scenes.h
+++ src/test_scenes.h
@@ -105,6 +105,82 @@ rt::Scene createScene_5() {
 }
 
 
+rt::Scene createScene_6() {
+    auto pinkMat = rt::createMaterial({1.0f, 0.0f, 1.0f}, 0.0f);
+    auto blueMat = rt::createMaterial({0.2f, 0.3f, 1.0f}, 0.0f);
+    auto brownMat = rt::createMaterial({0.8f, 0.5f, 0.2f}, 0.0f);
+    auto mirrorMat = rt::createMaterial({0.8f, 0.8f, 0.8f}, 1.0f);
+
+    auto sph1 = rt::createSphere({0.0f, 0.0f, 0.0f}, 1.0f, pinkMat);
+    auto sph2 = rt::createSphere({2.0f, 0.0f, 0.0f}, 1.0f, brownMat);
+    auto ground = rt::createSphere({0.0f, -101.0f, 0.0f}, 100.0f, blueMat);
+    auto triangle = rt::createTriangle({-1.0f, -1.0f, -1.0f}, {-1.0f, 1.0f, -1.0f}, {-1.5f, 0.0f, 2.0f}, mirrorMat);
+
+    rt::Scene scene;
+
+    scene.objects.push_back(sph1);
+    scene.objects.push_back(sph2);
+    scene.objects.push_back(ground);
+    scene.objects.push_back(triangle);
+
+    scene.backgroundColor = {230, 230, 250};
+    scene.backgroundColor /= 255.0f;
+
+    return scene;
+}
+
+
+rt::Scene createScene_7() {
+    auto greenMat = rt::createMaterial({0.2f, 0.6f, 0.0f}, 0.0f);
+    auto blueMat = rt::createMaterial({0.2f, 0.6f, 1.0f}, 0.0f);
+    auto mirrorMat = rt::createMaterial({0.7f, 0.7f, 0.7f}, 0.95f);
+
+    auto sph1 = rt::createSphere({0.0f, 0.0f, 0.0f}, 1.0f, blueMat);
+    auto ground = rt::createSphere({0.0f, -101.0f, 0.0f}, 100.0f, greenMat);
+    auto tri1 = rt::createTriangle({-2.0f, -1.0f, 2.0f}, {-2.0f, -1.0f, -2.0f}, {2.0f, -0.5f, -2.0f}, mirrorMat);
+    auto tri2 = rt::createTriangle({2.0f, -0.5f, -2.0f}, {2.0f, -0.5f, 2.0f}, {-2.0f, -1.0f, 2.0f}, mirrorMat);
+
+    rt::Scene scene;
+
+    scene.objects.push_back(sph1);
+    scene.objects.push_back(ground);
+    scene.objects.push_back(tri1);
+    scene.objects.push_back(tri2);
+
+    scene.backgroundColor = {104, 184, 235};
+    scene.backgroundColor /= 255.0f;
+
+    return scene;
+}
+
+
+rt::Scene createScene_8() {
+    auto pinkMat = rt::createMaterial({1.0f, 0.0f, 1.0f}, 0.0f);
+    auto blueMat = rt::createMaterial({0.2f, 0.3f, 1.0f}, 0.0f);
+    auto lightMat = rt::createEmissiveMaterial({0.8f, 0.5f, 0.2f}, 100.0f);
+    auto mirrorMat = rt::createMaterial({0.8f, 0.8f, 0.8f}, 1.0f);
+
+    auto sph = rt::createSphere({0.5f, 0.0f, 0.0f}, 1.0f, pinkMat);
+    auto ground = rt::createSphere({0.0f, -101.0f, 0.0f}, 100.0f, blueMat);
+    auto light = rt::createSphere({32.0f, 4.0f, -32.0f}, 20.0f, lightMat);
+    auto tri1 = rt::createTriangle({0.0f, 2.0f, -3.0f}, {-2.0f, 2.0f, -2.0f}, {0.0f, -1.0f, -3.0f}, mirrorMat);
+    auto tri2 = rt::createTriangle({-2.0f, 2.0f, -2.0f}, {0.0f, -1.0f, -3.0f}, {-2.0f, -1.0f, -2.0f}, mirrorMat);
+
+    rt::Scene scene;
+
+    scene.objects.push_back(sph);
+    scene.objects.push_back(ground);
+    scene.objects.push_back(light);
+    scene.objects.push_back(tri1);
+    scene.objects.push_back(tri2);
+
+    scene.backgroundColor = {70, 70, 70};
+    scene.backgroundColor /= 255.0f;
+
+    return scene;
+}
+
+
 std::vector<rt::internal::Scene> createAllScenes(cl::Context context, cl::CommandQueue queue) {
     std::vector<rt::Scene> scenes = {
         createScene_1(),
@@ -112,6 +188,9 @@ std::vector<rt::internal::Scene> createAllScenes(cl::Context context, cl::Comman
         createScene_3(),
         createScene_4(),
         createScene_5(),
+        createScene_6(),
+        createScene_7(),
+        createScene_8(),
     };
     std::vector<rt::internal::Scene> res;
     for (const auto& scene : scenes) {
